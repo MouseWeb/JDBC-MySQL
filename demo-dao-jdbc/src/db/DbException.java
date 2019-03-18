918a1b7c8a939e25f3f3ff61e3e4bbd00d916b49
Douglas Coelho <douglas@mouseweb.com.br>
2019-03-17 23:45:48 -0300

    demo DAO JDBC

---
+++ demo-dao-jdbc/src/db/DbException.java
@@ -0,0 +1,12 @@
+package db;
+
+// Class informa a mensagem de exceção da conexão .
+public class DbException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DbException(String msg) {
+		super(msg);
+	}
+	
+}
