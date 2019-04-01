f3c507654559c9b4578d3fd7d85f849963c9ef5f
Douglas Coelho <douglas@mouseweb.com.br>
2019-03-31 23:45:02 -0300

    findById implementation

--- demo-dao-jdbc/src/model/dao/DaoFactory.java
+++ demo-dao-jdbc/src/model/dao/DaoFactory.java
@@ -1,12 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	// è um a macete para não precisar expor a IMPL, deixando somente a interface.
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
 }
