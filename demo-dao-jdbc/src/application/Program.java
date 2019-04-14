ff6034601ec40af1e00476322a3c0a2cb2ad0d62
Douglas Coelho <douglas@mouseweb.com.br>
2019-04-14 18:37:27 -0300

    findAll implementantion

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -30,6 +30,13 @@ public class Program {
 
 		}
 		
+		System.out.println("\n=== TEST 3: seller findAll ===");
+	    list = sellerDao.findAll();
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+
+		}
 	}
 
 }
